b113dccadfd509a2248c2c57e41263939ffec64c
Diego Gutierrez <dgutierrez1287@gmail.com>
2021-08-29 21:19:07 -0400

    changed lua module naming to remove some abiquity

--- neovim/.config/nvim/lua/custom/telescope.lua
+++
@@ -1,5 +0,0 @@
-local actions = require("telescope.actions")
-
-require("telescope").load_extension("git_worktree")
-require("telescope").load_extension("fzy_native")
-require('telescope').load_extension('ultisnips')
